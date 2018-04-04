bb356485f603a1ac0f59a5cca06547c8860e2d6b
Netra Pathak <netra.20@gmail.com>
2018-04-04 14:24:21 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -9,6 +9,7 @@ import sys
 import urllib
 import scipy
 import pandas
+import difflib
 import os
 # import cv2
 # from sklearn.utils import shuffle
@@ -28,4 +29,17 @@ def writeToCsv(filePath,csvData):
        spamwriter.writerow(csvData)
        csvfile.flush()
        
- 
+def compareFiles(file_1, file_2):   #example:   file_1='file_1.py', file_2='./subdir/file_2.py'
+    with open(file_1) as text1:
+        with open(file_2) as text2:
+            d = difflib.Differ()
+            diff = list(d.compare(text1.readlines(), text2.readlines()))
+            with open('diff.txt', 'w') as diff_file:
+                _diff = ''.join(diff)
+                diff_file.write(_diff)
+    
+#     # The ndiff() function produces essentially the same output
+#     with open(file_1='text1.txt') as text1, open(file_2='text2.txt') as text2:
+#         diff = difflib.ndiff(text1.readlines(), text2.readlines())
+#     with open('diff.txt', 'w') as result:
+#         for line in diff: result.write(line)
